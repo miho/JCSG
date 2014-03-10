842ac346e36b4269a3bcfbf3636338ebac5108b9
Michael Hoffer <info@michaelhoffer.de>
2014-03-10 15:38:59 +0100

    auto-license header repair done

--- src/main/java/eu/mihosoft/vrl/v3d/Peg.java
+++ src/main/java/eu/mihosoft/vrl/v3d/Peg.java
@@ -1,8 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * Peg.java
+ *
+ * Copyright 2014-2014 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY Michael Hoffer <info@michaelhoffer.de> "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Michael Hoffer <info@michaelhoffer.de> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Michael Hoffer <info@michaelhoffer.de>.
+ */ 
 
 package eu.mihosoft.vrl.v3d;
 
@@ -11,7 +35,7 @@ package eu.mihosoft.vrl.v3d;
  * @author Michael Hoffer &lt;info@michaelhoffer.de&gt;
  */
 public class Peg {
-    
+
     public static CSG peg() {
         //      ol
         //     | |
@@ -75,6 +99,6 @@ public class Peg {
 
         return peg_points.
                 transformed(Transform.unity().translateX(-oo)).
-                transformed(Transform.unity().rotX(90).translateZ(-pd/2));
+                transformed(Transform.unity().rotX(90).translateZ(-pd / 2));
     }
 }
--- src/main/java/eu/mihosoft/vrl/v3d/Plane.java
+++ src/main/java/eu/mihosoft/vrl/v3d/Plane.java
@@ -1,52 +1,33 @@
-/* 
+/**
  * Plane.java
  *
- * Copyright (c) 2009–2014 Steinbeis Forschungszentrum (STZ Ölbronn),
- * Copyright (c) 2006–2014 by Michael Hoffer
- * 
- * This file is part of Visual Reflection Library (VRL).
+ * Copyright 2014-2014 Michael Hoffer <info@michaelhoffer.de>. All rights reserved.
  *
- * VRL is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * as published by the Free Software Foundation.
- * 
- * see: http://opensource.org/licenses/LGPL-3.0
- *      file://path/to/VRL/src/eu/mihosoft/vrl/resources/license/lgplv3.txt
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
  *
- * VRL is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *    1. Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
  *
- * This version of VRL includes copyright notice and attribution requirements.
- * According to the LGPL this information must be displayed even if you modify
- * the source code of VRL. Neither the VRL Canvas attribution icon nor any
- * copyright statement/attribution may be removed.
+ *    2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *       of conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
  *
- * Attribution Requirements:
+ * THIS SOFTWARE IS PROVIDED BY Michael Hoffer <info@michaelhoffer.de> "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL Michael Hoffer <info@michaelhoffer.de> OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- * If you create derived work you must do three things regarding copyright
- * notice and author attribution.
- *
- * First, the following text must be displayed on the Canvas or an equivalent location:
- * "based on VRL source code".
- * 
- * Second, the copyright notice must remain. It must be reproduced in any
- * program that uses VRL.
- *
- * Third, add an additional notice, stating that you modified VRL. In addition
- * you must cite the publications listed below. A suitable notice might read
- * "VRL source code modified by YourName 2012".
- * 
- * Note, that these requirements are in full accordance with the LGPL v3
- * (see 7. Additional Terms, b).
- *
- * Publications:
- *
- * M. Hoffer, C.Poliwoda, G.Wittum. Visual Reflection Library -
- * A Framework for Declarative GUI Programming on the Java Platform.
- * Computing and Visualization in Science, in press.
- */
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Michael Hoffer <info@michaelhoffer.de>.
+ */ 
+
 package eu.mihosoft.vrl.v3d;
 
 // # class Plane
