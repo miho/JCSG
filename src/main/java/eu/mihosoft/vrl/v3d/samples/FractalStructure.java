5da3e434365c2474de2a4d8ce11e868ecf9d281e
Christian Poliwoda <cpoliwoda@fidibus.gcsc.uni-frankfurt.de>
2014-08-04 11:33:40 +0200

    some cleaning

--- src/main/java/eu/mihosoft/vrl/v3d/samples/FractalStructure.java
+++ src/main/java/eu/mihosoft/vrl/v3d/samples/FractalStructure.java
@@ -55,142 +55,6 @@ public class FractalStructure {
 
     int level = 0;
 
-    /* BACKUP 20140801
-    
-     public FractalStructure(Vector3d groundCenter, Vector3d topCenter,
-     int numberOfGroundEdges, double thickness, int level) {
-
-     NextThickness = thickness / NextThicknessDivider;
-
-     if (numberOfGroundEdges < 3) {
-     numberOfGroundEdges = 3;
-     System.err.println("numberOfGroundEdges need to be at least 3 and is set therefore to 3.");
-     }
-     this.numberOfGroundEdges = numberOfGroundEdges;
-
-     this.level = level;
-
-     // save the centers
-     this.groundCenter = groundCenter;
-     this.topCenter = topCenter;
-
-     // create point lists
-     groundPoints = new ArrayList<Vector3d>();
-     topPoints = new ArrayList<Vector3d>();
-
-     // x, y, z
-     //the first point is the most in the north in the x-y-plane
-     Vector3d circlePoint = null;
-
-     double angel = 0.0;
-     double radians = 0.0;
-     double radius = thickness / 2.0;
-
-     Matrix3d rotationMatrix = null;
-
-     Vector3d rotationAxis = new Vector3d(
-     topCenter.x - groundCenter.x,
-     topCenter.y - groundCenter.y,
-     topCenter.z - groundCenter.z).normalized();
-
-     //helper to reduce space and increase a little bit performance
-     double OmCa = 0;
-     double cosA = 0;
-     double sinA = 0;
-     double x = rotationAxis.x;
-     double y = rotationAxis.y;
-     double z = rotationAxis.z;
-
-     //we need one starting point which we can rotate on the circle        
-     Vector3d orthoVecToRotAxis = null;
-     //        //http://www.gutefrage.net/tipp/orthogonalen-vektor-zu-einem-vektor-finden
-     //        if (z != 0) {
-     //            orthoVecToRotAxis = new Vector3d(y, x, -2 * x * y / z);
-     //        } else if (y != 0) {
-     //            orthoVecToRotAxis = new Vector3d(z, -2 * x * z / y, x);
-     //        } else if (x != 0) {
-     //            orthoVecToRotAxis = new Vector3d(-2 * y * z / x, z, y);
-     //        } else {
-     //            System.err.println("ERROR no orthogonal vector found");
-     //        }
-
-     if (!rotationAxis.equals(Vector3d.X_ONE)) {
-     orthoVecToRotAxis = rotationAxis.cross(Vector3d.X_ONE);
-
-     } else if (!rotationAxis.equals(Vector3d.Y_ONE)) {
-     orthoVecToRotAxis = rotationAxis.cross(Vector3d.Y_ONE);
-
-     } else if (!rotationAxis.equals(Vector3d.Z_ONE)) {
-     orthoVecToRotAxis = rotationAxis.cross(Vector3d.Z_ONE);
-     }
-
-     orthoVecToRotAxis = orthoVecToRotAxis.normalized().times(radius);
-
-     //        /// NEW TRY go in angle step sizes
-     //        angel = 360.0 / numberOfGroundEdges;
-
-     // add/create the points around the center 
-     for (int i = 0; i < numberOfGroundEdges; i++) {
-
-     angel = 360.0 * i / numberOfGroundEdges;
-     radians = (angel - 90) * Math.PI / 180;
-     //            angel *= i;
-
-     //            OmCa = (1 - Math.cos(angel));
-     //            cosA = Math.cos(angel);
-     //            sinA = Math.sin(angel);
-     //
-     //            //http://de.wikipedia.org/wiki/Drehmatrix#Drehmatrizen_des_Raumes_R.C2.B3
-     //            rotationMatrix = new Matrix3d(
-     //                    x * x * OmCa + cosA, x * y * OmCa - z * sinA, x * z * OmCa + y * sinA,
-     //                    y * x * OmCa + z * sinA, y * y * OmCa + cosA, y * z * OmCa - x * sinA,
-     //                    z * x * OmCa - y * sinA, z * y * OmCa + x * sinA, z * z * OmCa + cosA);
-     //
-     //            circlePoint = rotationMatrix.times(orthoVecToRotAxis);
-     //
-     //            //ground points
-     //            groundPoints.add(circlePoint.plus(groundCenter));
-
-     circlePoint = new Vector3d(
-     groundCenter.x + radius * Math.cos(radians),
-     groundCenter.y + radius * Math.sin(radians),
-     groundCenter.z);
-            
-     groundPoints.add(circlePoint);
-     System.out.println("ground circlePoint: " + circlePoint);
-
-     //top points
-     //            topPoints.add(circlePoint.plus(topCenter));
-
-     circlePoint = new Vector3d(
-     topCenter.x + radius * Math.cos(radians),
-     topCenter.y + radius * Math.sin(radians),
-     topCenter.z);
-     topPoints.add(circlePoint);
-     }
-
-     //the last points in the list are the center points 
-     groundPoints.add(groundCenter);
-
-     topPoints.add(topCenter);
-
-     //here we want to save the substructures
-     subStructures = new ArrayList<>();
-
-     if (level == 0) {
-     subStructures.add(createStructure());
-     } else {
-     ArrayList<FractalStructure> subFractals = createSubStructures();
-
-     for (int i = 0; i < subFractals.size(); i++) {
-     subStructures.add(subFractals.get(i).toCSG());
-
-     }
-     }
-
-     }
-    
-     */
     public FractalStructure(Vector3d groundCenter, Vector3d topCenter,
             int numberOfGroundEdges, double thickness, int level) {
 
@@ -230,7 +94,7 @@ public class FractalStructure {
 
         System.out.println(" orthoVecToRotAxis1 = " + orthoVecToRotAxis1);
         System.out.println(" orthoVecToRotAxis2 = " + orthoVecToRotAxis2);
-        
+
         // x, y, z
         //the first point is the most in the north in the x-y-plane
         Vector3d circlePoint = null;
@@ -242,8 +106,8 @@ public class FractalStructure {
         double x = 0;
         double y = 0;
 
-        System.out.println(" angelStepSize = "+ angelStepSize);
-        
+        System.out.println(" angelStepSize = " + angelStepSize);
+
         // add/create the points around the ground and top center 
         for (int i = 0; i < numberOfGroundEdges; i++) {
 
@@ -252,30 +116,27 @@ public class FractalStructure {
             x = radius * Math.cos(radians);
             y = radius * Math.sin(radians);
 
-            System.out.println(" angel = "+ angel);
-            System.out.println(" radians = "+ radians);
-            System.out.println(" Math.cos(radians) = "+ Math.cos(radians));
-            System.out.println(" Math.sin(radians) = "+ Math.sin(radians));
-            
-            
-            
+            System.out.println(" angel = " + angel);
+            System.out.println(" radians = " + radians);
+            System.out.println(" Math.cos(radians) = " + Math.cos(radians));
+            System.out.println(" Math.sin(radians) = " + Math.sin(radians));
+
             // Plane equation E(x,y) = S + P * x + Q * y
             // with P,Q orthogonal to the center rotation axis and
             // with x,y from the cirlce gives use the cirlce in 3d space
-            
             //ground points
-            System.out.println("groundCenter = "+ groundCenter);
-            System.out.println("orthoVecToRotAxis1.times(x) = "+ orthoVecToRotAxis1.times(x));
-            System.out.println("orthoVecToRotAxis2.times(y) = "+ orthoVecToRotAxis2.times(y));
-            
+            System.out.println("groundCenter = " + groundCenter);
+            System.out.println("orthoVecToRotAxis1.times(x) = " + orthoVecToRotAxis1.times(x));
+            System.out.println("orthoVecToRotAxis2.times(y) = " + orthoVecToRotAxis2.times(y));
+
             circlePoint = groundCenter.plus(orthoVecToRotAxis1.times(x)).plus(orthoVecToRotAxis2.times(y));
 
             groundPoints.add(circlePoint);
-            System.out.println("ground circlePoint "+i+": " + circlePoint);
+            System.out.println("ground circlePoint " + i + ": " + circlePoint);
 
             //top points
             circlePoint = topCenter.plus(orthoVecToRotAxis1.times(x)).plus(orthoVecToRotAxis2.times(y));
-            
+
             topPoints.add(circlePoint);
         }
 
@@ -426,7 +287,7 @@ public class FractalStructure {
 //        frac.collectGroundAndTopPoints();
         CSG csg = new FractalStructure(Vector3d.ZERO, Vector3d.Z_ONE, 6, 4, 0).createStructure();
 //        CSG csg = new FractalStructure(Vector3d.ZERO, Vector3d.Z_ONE, 7, 2, 1).toCSG();
-//        CSG csg = new FractalStructure(Vector3d.ZERO, new Vector3d(5, 5, 1), 7, 2, 2).toCSG();
+//        CSG csg = new FractalStructure(new Vector3d(-1, -1, -1), new Vector3d(1, 1, 1), 7, 4, 3).toCSG();
 
         FileUtil.write(Paths.get("fractal-structure.stl"), csg.toStlString());
 
@@ -435,89 +296,3 @@ public class FractalStructure {
     }
 
 }
-
-//    public FractalStructure() {
-//
-//        // x, y, z
-//        Vector3d p1 = new Vector3d(0, 1, 0);
-//        Vector3d p2 = new Vector3d(-1, 0, 0);
-//        Vector3d p3 = new Vector3d(1, 0, 0);
-//
-//        Vector3d p4 = new Vector3d(0, 1, 1);
-//        Vector3d p5 = new Vector3d(-1, 0, 1);
-//        Vector3d p6 = new Vector3d(1, 0, 1);
-//
-//        Polygon ground = Polygon.fromPoints(p1, p2, p3);
-//        Polygon leftSide = Polygon.fromPoints(p1, p2, p5, p4);
-//        Polygon groundSide = Polygon.fromPoints(p2, p3, p6, p5);
-//        Polygon rightSide = Polygon.fromPoints(p1, p4, p6, p3);
-//        Polygon top = Polygon.fromPoints(p4, p5, p6);
-//
-//        polyeder = CSG.fromPolygons(ground, leftSide, groundSide, rightSide, top);
-//    }
-//
-//    private void collectGroundAndTopPoints() {
-//        groundPoints = new ArrayList<Vector3d>();
-//
-//        //the ground points
-//        List vertexList = polyeder.getPolygons().get(0).vertices;
-//
-//        // helpers to calculate the center
-//        double cx = 0;
-//        double cy = 0;
-//        double cz = 0;
-//
-//        Vector3d tmp = null;
-//        int size = vertexList.size();
-//
-//        for (int i = 0; i < size; i++) {
-//            tmp = ((Vertex) vertexList.get(i)).pos;
-//
-//            //collect the edges of the ground
-//            groundPoints.add(tmp);
-//
-//            //first part calculate the center of the ground
-//            cx += tmp.x;
-//            cy += tmp.y;
-//            cz += tmp.z;
-//        }
-//        //second part calculate the center of the ground
-//        tmp.x = cx / size;
-//        tmp.y = cy / size;
-//        tmp.z = cz / size;
-//
-//        // add center of the ground to the ground points
-//        groundPoints.add(tmp);
-//
-//        int polySize = polyeder.getPolygons().size();
-//
-//        // the top points
-//        vertexList = polyeder.getPolygons().get(polySize - 1).vertices;
-//
-////        did not need to recalculate because ground and top 
-////        need to have the same count of points
-////        size = vertexList.size();
-//        // reset/clear helpers 
-//        cx = 0;
-//        cy = 0;
-//        cz = 0;
-//
-//        for (int i = 0; i < size; i++) {
-//            tmp = ((Vertex) vertexList.get(i)).pos;
-//
-//            //collect the edges of the ground
-//            topPoints.add(tmp);
-//
-//            //first part calculate the center of the ground
-//            cx += tmp.x;
-//            cy += tmp.y;
-//            cz += tmp.z;
-//        }
-//        //second part calculate the center of the ground
-//        tmp.x = cx / size;
-//        tmp.y = cy / size;
-//        tmp.z = cz / size;
-//
-//        // add center of the ground to the ground points
-//        topPoints.add(tmp);
-//    }
