7026fbe412f4cd8f23f72b11dd1426b2a6e5634e
Michael Hoffer <info@michaelhoffer.de>
2014-10-04 17:22:48 +0200

    convex hull component for vrl

---
+++ src/main/java/eu/mihosoft/vrl/v3d/JCSGPluginConfigurator.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.mihosoft.vrl.v3d;
+
+import eu.mihosoft.vrl.system.InitPluginAPI;
+import eu.mihosoft.vrl.system.PluginAPI;
+import eu.mihosoft.vrl.system.PluginIdentifier;
+import eu.mihosoft.vrl.system.VPluginAPI;
+import eu.mihosoft.vrl.system.VPluginConfigurator;
+
+/**
+ *
+ * @author Michael Hoffer &lt;info@michaelhoffer.de&gt;
+ */
+public class JCSGPluginConfigurator extends VPluginConfigurator{  
+
+    public JCSGPluginConfigurator() {
+          //specify the plugin name and version
+       setIdentifier(new PluginIdentifier("JCSG", "0.1"));
+
+       // optionally allow other plugins to use the api of this plugin
+       // you can specify packages that shall be
+       // exported by using the exportPackage() method:
+       //
+       exportPackage("eu.mihosoft.vrl.v3d");
+
+       // describe the plugin
+       setDescription("JCSG Plugin (Constructive Solid Geometry)");
+
+       // copyright info
+       setCopyrightInfo("JCSG",
+               "(c) Michael Hoffer",
+               "www.mihosoft.eu", "BSD", "License Text...");
+
+       // specify dependencies
+       // addDependency(new PluginDependency("VRL", "0.4.0", "0.4.0"));
+    }
+    
+
+    @Override
+    public void register(PluginAPI api) {
+       // register plugin with canvas
+       if (api instanceof VPluginAPI) {
+           VPluginAPI vapi = (VPluginAPI) api;
+
+           // Register visual components:
+           //
+           // Here you can add additional components,
+           // type representations, styles etc.
+           //
+           // ** NOTE **
+           //
+           // To ensure compatibility with future versions of VRL,
+           // you should only use the vapi or api object for registration.
+           // If you directly use the canvas or its properties, please make
+           // sure that you specify the VRL versions you are compatible with
+           // in the constructor of this plugin configurator because the
+           // internal api is likely to change.
+           //
+           // examples:
+           //
+           // vapi.addComponent(MyComponent.class);
+           // vapi.addTypeRepresentation(MyType.class);
+           
+           vapi.addComponent(PointCloud2ConvexHull.class);
+       }
+   }
+
+    @Override
+   public void unregister(PluginAPI api) {
+       // nothing to unregister
+   }
+
+    @Override
+    public void init(InitPluginAPI iApi) {
+       // nothing to init
+   }
+ }
---
+++ src/main/java/eu/mihosoft/vrl/v3d/PointCloud2ConvexHull.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.mihosoft.vrl.v3d;
+
+import eu.mihosoft.vrl.annotation.ComponentInfo;
+import eu.mihosoft.vrl.annotation.ParamInfo;
+import eu.mihosoft.vrl.io.IOUtil;
+import eu.mihosoft.vrl.v3d.ext.quickhull3d.HullUtil;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Michael Hoffer &lt;info@michaelhoffer.de&gt;
+ */
+@ComponentInfo(name = "PointCloud2ConvexHull", category = "JCSG")
+public class PointCloud2ConvexHull implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public void hull(
+            @ParamInfo(name = "List<Vector3d> points") List<Vector3d> points,
+            @ParamInfo(style = "save-dialog", options = "endings=[\".stl\"]; description=\".stl File\"") File outputFile) throws IOException {
+        CSG result = HullUtil.hull(points, new PropertyStorage());
+
+        FileUtil.write(outputFile, result.toStlString());
+    }
+
+    public void hull(
+            @ParamInfo(style = "load-dialog") File pointFile,
+            @ParamInfo(style = "save-dialog", options = "endings=[\".stl\"]; description=\".stl File\"") File outputFile) throws IOException {
+
+        List<String> lines = IOUtil.readFileToStringList(pointFile);
+
+        List<Vector3d> points = new ArrayList<Vector3d>(lines.size());
+
+        int lineCount = -1;
+
+        for (String l : lines) {
+            lineCount++;
+            if (l.trim().startsWith("#")) {
+                continue;
+            }
+
+            String[] coordString = l.split(" ");
+
+            if (coordString.length != 3) {
+                throw new RuntimeException(
+                        "Syntax error in line " + lineCount + ": too many entries.");
+            }
+            try {
+                Vector3d vec = new Vector3d(
+                        Double.parseDouble(coordString[0]),
+                        Double.parseDouble(coordString[1]),
+                        Double.parseDouble(coordString[2]));
+                points.add(vec);
+            } catch (NumberFormatException ex) {
+                throw new RuntimeException(
+                        "Syntax error in line " + lineCount + ": illegal number format.");
+            }
+        }
+
+        CSG result = HullUtil.hull(points, new PropertyStorage());
+
+        FileUtil.write(outputFile, result.toStlString());
+    }
+
+    public CSG hull(
+            @ParamInfo(name = "List<Vector3d> points") List<Vector3d> points) {
+        return HullUtil.hull(points, new PropertyStorage());
+    }
+}
