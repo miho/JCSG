1d2c0aac7106d0914af846cad191620720e9dc36
Michael Hoffer <info@michaelhoffer.de>
2014-11-12 17:20:15 +0100

    TriMail example

--- src/main/java/eu/mihosoft/vrl/v3d/samples/TriMail.java
+++ src/main/java/eu/mihosoft/vrl/v3d/samples/TriMail.java
@@ -16,19 +16,20 @@ import java.nio.file.Paths;
  *
  * @author Michael Hoffer &lt;info@michaelhoffer.de&gt;
  */
-public class PolyMail {
+public class TriMail {
 
     public CSG toCSG(int numEdges, int numX, int numY) {
 
-        PolyMailTile tile = new PolyMailTile().setNumEdges(numEdges);
+        PolyMailTile tile = new PolyMailTile().setNumEdges(numEdges).
+                setPinThickness(2.1).
+                setHingeHoleScale(1.2).setConeLength(1.8);
 
-        double jointRadius = tile.getJointRadius();
         double hingeHoleScale = tile.getHingeHoleScale();
 
-        CSG malePart = tile.setMale().toCSG();
-        CSG femalePart = tile.setFemale().toCSG();
-
-        Vector3d malePartBounds = malePart.getBounds().getBounds();
+        CSG malePart = tile.setMale().toCSG().transformed(
+                            unity().rotZ(360.0 / numEdges*0.75));
+        CSG femalePart = tile.setFemale().toCSG().transformed(
+                            unity().rotZ(360.0 / numEdges*0.25));
 
         CSG result = null;
 
@@ -41,27 +42,29 @@ public class PolyMail {
                         - tile.getJointRadius());
 
                 double xOffset = 0;
+                double yOffset =0;
 
                 if (y % 2 == 0) {
-                    xOffset = tile.getApothem() + pinOffset * 0.5;
+                    xOffset = tile.getSideLength() * 0.5+ pinOffset*0.5;
+                    
+                    if ((y/2) % 2 == 0) {
+                        xOffset-= tile.getSideLength()*0.5 + pinOffset*0.5;
+                    }
+                    
+                     yOffset = +tile.getPinLength()*3;
                 }
-
+                
+                
                 double translateX
-                        = (-tile.getApothem() * 2 - pinOffset) * x + xOffset;
-                double translateY
-                        = (-tile.getRadius() * 0.5 - tile.getRadius()) * y;
+                        = (-tile.getSideLength() - pinOffset) * x + xOffset;
+                double translateY = tile.getRadius()*y - yOffset;
 
                 CSG part2;
 
-                if (x % 2 == 0) {
+                if (y % 2 == 0) {
                     part2 = femalePart.clone();
                 } else {
                     part2 = malePart.clone();
-                }
-
-                if (numEdges % 2 != 0) {
-                    part2 = part2.transformed(
-                            unity().rotZ(360.0 / numEdges * 0.5));
                 }
 
                 part2 = part2.transformed(
@@ -79,6 +82,6 @@ public class PolyMail {
     }
 
     public static void main(String[] args) throws IOException {
-        FileUtil.write(Paths.get("polymail.stl"), new PolyMail().toCSG(3, 3, 2).toStlString());
+        FileUtil.write(Paths.get("trimail.stl"), new TriMail().toCSG(3, 3, 3).toStlString());
     }
 }
