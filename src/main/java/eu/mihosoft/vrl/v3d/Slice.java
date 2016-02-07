6d014a5b7f316da1891141ecc08ce04008ceb965
madhephaestus <mad.hephaestus@gmail.com>
2016-02-07 15:59:22 -0500

    slicing engine and storage for the slicing objects definesd.

---
+++ src/main/java/eu/mihosoft/vrl/v3d/Slice.java
@@ -0,0 +1,28 @@
+package eu.mihosoft.vrl.v3d;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Slice  {
+	private static ISlice sliceEngine = new ISlice() {
+		
+		@Override
+		public List<Vector3d> slice(CSG incoming, Plane slicePlane, double normalInsetDistance) {
+			List<Vector3d> slicedPoints = new ArrayList<>();
+			System.out.println("This is a dummy slice engine and is not implemented yet");
+			return slicedPoints;
+		}
+	};
+	public static  List<Vector3d> slice(CSG incoming, Plane slicePlane, double normalInsetDistance) {
+		return getSliceEngine().slice(incoming, slicePlane, normalInsetDistance);
+	}
+	
+	
+	public static ISlice getSliceEngine() {
+		return sliceEngine;
+	}
+	public static void setSliceEngine(ISlice sliceEngine) {
+		Slice.sliceEngine = sliceEngine;
+	}
+
+}
