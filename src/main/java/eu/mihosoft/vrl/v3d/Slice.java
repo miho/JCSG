ab7d2c661a873e0aade54823452dcd140c947d38
Octogonapus <firey45@gmail.com>
2016-05-30 15:59:08 -0400

    Added initial Slice method implementation and changed ISlice interface to take a slice plane as a double for issue #21. Also added a unit test for the slice method.

--- src/main/java/eu/mihosoft/vrl/v3d/Slice.java
+++ src/main/java/eu/mihosoft/vrl/v3d/Slice.java
@@ -1,19 +1,26 @@
 package eu.mihosoft.vrl.v3d;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class Slice  {
-	private static ISlice sliceEngine = new ISlice() {
-		
-		@Override
-		public List<Vector3d> slice(CSG incoming, Transform slicePlane, double normalInsetDistance) {
-			List<Vector3d> slicedPoints = new ArrayList<>();
-			System.out.println("This is a dummy slice engine and is not implemented yet");
-			return slicedPoints;
+public class Slice
+{
+	private static final CSG planeCSG = new Cube(10000, 10000, 1).noCenter().toCSG().movex(-5000).movey(-5000);
+
+	private static ISlice sliceEngine = (incoming, slicePlane, normalInsetDistance) -> {
+        List<Vector3d> slicedPoints = new ArrayList<>();
+
+		for (Polygon polygon : incoming.movez(-slicePlane).intersect(planeCSG).getPolygons())
+		{
+			slicedPoints.addAll(polygon.vertices.stream().filter(v -> v.getZ() == 0).map(v -> new Vector3d(v.getX(), v.getY(), v.getZ())).collect(Collectors.toList()));
 		}
-	};
-	public static  List<Vector3d> slice(CSG incoming, Transform slicePlane, double normalInsetDistance) {
+
+        return slicedPoints;
+    };
+
+	public static  List<Vector3d> slice(CSG incoming, double slicePlane, double normalInsetDistance) {
 		return getSliceEngine().slice(incoming, slicePlane, normalInsetDistance);
 	}
 	
---
+++ src/test/java/eu/mihosoft/vrl/v3d/SliceTest.java
@@ -0,0 +1,37 @@
+package eu.mihosoft.vrl.v3d;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ryan Benasutti on 5/30/2016.
+ */
+
+public class SliceTest
+{
+    @Test
+    public void slice() throws Exception
+    {
+        //Create a CSG to slice
+        CSG sliced = new Cube(10, 10, 10).toCSG().union(new Sphere(5).toCSG());
+
+        //Get a slice
+        List<Vector3d> points = Slice.slice(sliced, 0, 0);
+
+        //Construct a Polygon from that slice
+        Polygon polygon = Polygon.fromPoints(points);
+
+        //Collect that Polygon into a List
+        List<Polygon> polygons = new ArrayList<>();
+        polygons.add(polygon);
+
+        //Construct a CSG from that Polygon List
+        CSG finished = CSG.fromPolygons(polygons);
+    }
+
+}
