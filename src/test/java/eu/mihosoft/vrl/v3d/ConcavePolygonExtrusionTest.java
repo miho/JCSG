891b7430082e003b337f210f2e740306afd2ce40
hephaestus <hephaestus@zoomtop>
2016-11-09 18:08:05 -0500

    #15 Adding a Unit test to demonstrate the issue
    This unit test is for testing concave polygons with the extrusion
    feature.

---
+++ src/test/java/eu/mihosoft/vrl/v3d/ConcavePolygonExtrusionTest.java
@@ -0,0 +1,14 @@
+package eu.mihosoft.vrl.v3d;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConcavePolygonExtrusionTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
