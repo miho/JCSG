b8011ce2eaabe8d68e7ba972ce0c04b344bf1116
Kevin Harrington <mad.hephaestus@gmail.com>
2016-11-20 17:02:17 -0500

    Working SVG loading with holes and islands in holes

--- src/main/java/eu/mihosoft/vrl/v3d/svg/SVGLoad.java
+++ src/main/java/eu/mihosoft/vrl/v3d/svg/SVGLoad.java
@@ -1,5 +1,4 @@
 package eu.mihosoft.vrl.v3d.svg;
-
 import java.io.File;
 import java.io.IOException;
 
@@ -30,22 +29,25 @@ import eu.mihosoft.vrl.v3d.CSG;
 import eu.mihosoft.vrl.v3d.Extrude;
 import eu.mihosoft.vrl.v3d.Polygon;
 import eu.mihosoft.vrl.v3d.Vector3d;
-
+//CSG.setDefaultOptType(CSG.OptType.CSG_BOUND);
 /**
  * Responsible for converting all SVG path elements into MetaPost curves.
  */
-public class SVGLoad {
+public class  SVGLoad {
 	private static final String PATH_ELEMENT_NAME = "path";
 	private static final String GROUP_ELEMENT_NAME = "g";
 	private Document svgDocument;
-
+	boolean holePolarity = true;
+	public void setHolePolarity(boolean p){
+		holePolarity = p;
+	}
 	/**
 	 * Responsible for converting an SVG path element to MetaPost. This will
 	 * convert just the bezier curve portion of the path element, not its style.
 	 * Typically the SVG path data is provided from the "d" attribute of an SVG
 	 * path node.
 	 */
-	class MetaPostPath {
+	class MetaPostPath2 {
 		private SVGOMPathElement pathElement;
 		private String transform;
 
@@ -57,8 +59,7 @@ public class SVGLoad {
 		 *            The path node containing a "d" attribute (output as
 		 *            MetaPost code).
 		 */
-		public MetaPostPath(Node pathNode, String transform) {
-			this.transform = transform;
+		public MetaPostPath2(Node pathNode) {
 			setPathNode(pathNode);
 		}
 
@@ -75,16 +76,19 @@ public class SVGLoad {
 			// String offset = pathElement.getOwnerSVGElement();
 
 			int pathObjects = pathList.getNumberOfItems();
-			// sb.append( "M "+offset
-			// .replaceAll("translate", "")
-			// .replaceAll("(", "")
-			// .replaceAll(")", "")
-			// +"\n");
-			// sb.append( "//"+getId()+"\n");
+			/*
+			 sb.append( "M "+offset
+			 .replaceAll("translate", "")
+			 .replaceAll("(", "")
+			 .replaceAll(")", "")
+			 +"\n");
+			 */
+			 //sb.append( "//"+getId()+"\n");
 
 			for (int i = 0; i < pathObjects; i++) {
 				SVGItem item = (SVGItem) pathList.getItem(i);
-				sb.append(String.format("%s%n", item.getValueAsString()));
+				String itemLine = String.format("%s%n", item.getValueAsString());
+				sb.append(itemLine);
 			}
 
 			return sb.toString();
@@ -158,6 +162,172 @@ public class SVGLoad {
 	public static ArrayList<CSG> extrude(URI uri, double thickness, double resolution) throws IOException{
 		return new SVGLoad(uri).extrude( thickness, resolution);
 	}
+	
+
+	private void loadAllGroups(NodeList pn,ArrayList<CSG> sections, double thickness, double resolution,double mvx ,double mvy){
+		//println "Loading groups from "+pn.getClass()
+		int pnCount = pn.getLength();
+		for (int j = 0; j < pnCount; j++) {
+			if(SVGOMGElement.class.isInstance(pn.item(j))){
+				SVGOMGElement element = (SVGOMGElement) pn.item(j);
+				NodeList pathNodes = element.getElementsByTagName("path");
+				Node transforms = element.getAttributes().getNamedItem("transform");
+				Node id = element.getAttributes().getNamedItem("id");
+				NodeList subgroups = element.getElementsByTagName("g");
+				// System.out.println(pathNodes.getClass());
+				String transformValue = "translate(0,0)";
+				if (transforms != null){
+					transformValue=transforms.getNodeValue();
+				}
+				if(transformValue.contains("matrix")){
+					transformValue = "translate(0,0)";
+					//println "Replacing matrix"
+				}
+				
+				String []transformValues = transformValue
+					 .replaceAll("translate", "")
+					 .replaceAll("\\(", "")
+					 .replaceAll("\\)", "")
+					 .split("\\,");
+				System.out.println(id.getNodeValue() +" "+transformValues );
+				mvx = Double.parseDouble(transformValues[0]);
+				mvy = Double.parseDouble(transformValues[1]);
+
+				if(subgroups!=null){
+					if(subgroups.getLength()<1){
+						//loadAllGroups(subgroups, sections, thickness, resolution, mvx, mvy);
+						//break;
+						//println "NO SubGroups"
+						
+						loadAllPaths((SVGOMGElement)pn.item(j), sections, thickness, resolution, mvx, mvy);
+						
+					}else{
+						//println "Has "+subgroups.getLength()+" SubGroups"
+					}
+				}
+				
+				
+				
+				
+			}
+//			else
+//				println "UNKNOWN ELEMENT "+pn.item(j).getClass()
+		}
+
+	}
+//SVGOMGElement
+	private void loadAllPaths(SVGOMGElement element,ArrayList<CSG> sections,double thickness, double resolution,double mvx ,double mvy){
+		//System.out.println("List of groups " + element.getClass());
+		ArrayList<CSG> holes = new ArrayList<CSG>();
+	
+			
+		NodeList pathNodes = element.getElementsByTagName("path");
+		//Node transforms = element.getAttributes().getNamedItem("transform");
+
+		if (pathNodes != null) {
+			
+			
+			ArrayList<Vector3d> p = new ArrayList<Vector3d>();
+			double connectorDepth = thickness;
+			int pathNodeCount = pathNodes.getLength();
+			//BowlerStudioController.setCsg(null,null);
+			//println "Loading paths: "+pathNodeCount
+			for (int iPathNode = 0; iPathNode < pathNodeCount; iPathNode++) {
+				
+				Node pathNode=pathNodes.item(iPathNode);
+				MetaPostPath2 mpp = new MetaPostPath2(pathNode);
+				String code = mpp.toCode();
+				
+				loadComposite( code, sections,thickness,resolution,mvx,mvy);
+			}
+		}
+		
+	}
+
+	private void loadComposite(String code,ArrayList<CSG> sections,double thickness, double resolution,double mvx ,double mvy){
+		ArrayList<CSG> holes =new ArrayList<CSG> ();
+		
+		//Count the occourences of M
+		int count = code.length() - code.replace("M", "").length();
+		if(count<2){
+			//println "Single path found"
+			
+			setHolePolarity(true);
+			try{
+				loadSingle(code,sections,holes,thickness,resolution,mvx,mvy);
+			}catch(Exception ex){
+				//BowlerStudio.printStackTrace(ex);
+			}
+		}else{
+			
+			setHolePolarity(false);
+			String [] pathParts = code.split("M");
+			//println "Complex path found "+ pathParts.length ;
+			ArrayList<CSG> tmp =new ArrayList<CSG> ();
+			for(int i=0;i<pathParts.length;i++){
+				String sectionedPart = "M"+pathParts[i];
+				// reapply the split char
+				if(sectionedPart.length()>1){
+					
+					//println "Seperated complex: "
+					loadSingle(sectionedPart,holes,tmp,thickness,resolution,mvx,mvy);
+				}
+			}
+			for(int i=0;i<tmp.size();i++){	
+						
+				for(CSG h:holes){
+					CSG cut = tmp.get(i).difference(h);
+					if(cut.getPolygons().size()>0){
+						// only apply cuts that to not obliterate the object, this should preserve islands
+						tmp.set(i,cut);
+					}
+				}
+				
+				
+			}
+			sections.addAll(tmp);
+			//sections.addAll(holes)
+		}
+		System.out.println("SVG has this many elements loaded: "+sections.size());
+		//BowlerStudioController.setCsg(sections,null);
+	}
+
+	private void loadSingle(String code,ArrayList<CSG> sections,ArrayList<CSG> holes,double thickness, double resolution, double mvx ,double mvy){
+		//println code
+		BezierPath path = new BezierPath();
+		path.parsePathString(code);
+		ArrayList<Vector3d> p = new ArrayList<Vector3d>();
+		for (double i = 0; i < 1.0; i += resolution) {
+			Vector3d point = path.eval((float) i);
+			// println point
+			p.add(point);
+		}
+		p.add(path.eval((float) (1.0-resolution/2.0)));
+		//System.out.println(" Path " + code);
+		boolean hole = Extrude.isCCW(Polygon.fromPoints(p));
+		if(!holePolarity)
+			hole=!hole;
+		try {
+			CSG newbit = Extrude.points(new Vector3d(0, 0, thickness), p)
+						.movex(mvx)
+						.movey(mvy)
+						.scale(0.376975);
+			if (!hole){
+				//println "NOT hole"
+				sections.add(newbit);
+			}else{
+				//println "Hole"
+				holes.add(newbit);
+			}
+			//
+			//
+		} catch (Exception ex) {
+			// System.out.println(" Path "+code );
+			//BowlerStudio.printStackTrace(ex);
+			// 
+		}
+	}
+	
 	public  ArrayList<CSG> extrude( double thickness, double resolution) throws IOException {
 
 		/**
@@ -173,62 +343,10 @@ public class SVGLoad {
 		// println "Loading converter"
 		NodeList pn = getSVGDocument().getDocumentElement().getElementsByTagName("g");
 		;
-		System.out.println("List of groups " + pn.getClass());
+		//System.out.println("List of groups " + pn.getClass());
 		ArrayList<CSG> sections = new ArrayList<CSG>();
-		ArrayList<CSG> holes = new ArrayList<CSG>();
-		int pnCount = pn.getLength();
-		for (int j = 0; j < pnCount; j++) {
-			SVGOMGElement element = (SVGOMGElement) pn.item(j);
-			NodeList pathNodes = element.getElementsByTagName("path");
-			Node transforms = element.getAttributes().getNamedItem("transform");
+		loadAllGroups( pn,sections,  thickness,  resolution,0,0);
 
-			if (pathNodes != null) {
-				// System.out.println(pathNodes.getClass());
-				// String
-				if (transforms != null)
-					System.out.println(transforms.getNodeValue() + " " + transforms.getClass());
-				// else
-
-				ArrayList<Vector3d> p = new ArrayList<Vector3d>();
-				double connectorDepth = thickness;
-				int pathNodeCount = pathNodes.getLength();
-				// BowlerStudioController.setCsg(null,null);
-				for (int iPathNode = 0; iPathNode < pathNodeCount; iPathNode++) {
-					String transform =transforms.getNodeValue();
-					Node pathNode=pathNodes.item(iPathNode);
-					MetaPostPath mpp = new MetaPostPath(pathNode, transform);
-					String code = mpp.toCode();
-
-					BezierPath path = new BezierPath();
-					path.parsePathString(code);
-					p.clear();
-					for (double i = 0; i < 1.0; i += resolution) {
-						Vector3d point = path.eval((float) i);
-						// println point
-						p.add(point);
-					}
-					p.add(path.eval((float) 1));
-					System.out.println(" Path " + code);
-					boolean hole = !Extrude.isCCW(Polygon.fromPoints(p));
-					try {
-						CSG newbit = Extrude.points(new Vector3d(0, 0, connectorDepth), p);
-						if (!hole)
-							sections.add(newbit);
-						else
-							holes.add(newbit);
-						// BowlerStudioController.addCsg(newbit);
-						//
-					} catch (Exception ex) {
-						// System.out.println(" Path "+code );
-						// BowlerStudio.printStackTrace(ex);
-						// ThreadUtil.wait(100);
-					}
-				}
-			}
-
-		}
-
-		sections.addAll(holes);
 		return sections;
 	}
 
@@ -314,19 +432,6 @@ public class SVGLoad {
 	private Document createSVGDocument(URI uri) throws IOException {
 		String parser = XMLResourceDescriptor.getXMLParserClassName();
 		SAXSVGDocumentFactory factory = new SAXSVGDocumentFactory(parser);
-		return factory.createDocument(uri.getPath());
-	}
-
-	/**
-	 * Reads a file and parses the path elements.
-	 * 
-	 * @param args
-	 *            args[0] - Filename to parse.
-	 * @throws IOException
-	 *             Error reading the SVG file.
-	 */
-	public static void main(String args[]) throws IOException {
-		URI uri = new File("simplified logo.svg").toURI();
-		new SVGLoad(uri).extrude( 20);
+		return factory.createDocument(uri.toString());
 	}
 }
