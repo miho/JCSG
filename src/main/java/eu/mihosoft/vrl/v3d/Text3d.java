1e9170d15d4cdb58e4041d8825cd298151e6ce8f
harrington <harrington@ROBOTICS01.admin.wpi.edu>
2016-12-14 15:01:58 -0500

    Removing the FXyz uses in Text3d

--- src/main/java/eu/mihosoft/vrl/v3d/Text3d.java
+++ src/main/java/eu/mihosoft/vrl/v3d/Text3d.java
@@ -38,10 +38,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.fxyz.shapes.primitives.Text3DMesh;
-import org.fxyz.shapes.primitives.TexturedMesh;
-import org.fxyz.utils.MeshUtils;
-
 /**
  * 3d text primitive. 
  * 
@@ -55,7 +51,6 @@ public class Text3d implements Primitive {
     private String fontName;
     private double size;
     private double depth;
-    Text3DMesh t3dMesh;
     private boolean noCenter;
 
     // 08.11.2016
@@ -100,13 +95,14 @@ public class Text3d implements Primitive {
         int realSize = (int) (fontSize * scaleFactor);
         double realHeight = depth * scaleFactor;
 
-        t3dMesh = new Text3DMesh(
-                text, fontName, realSize, false, realHeight, 0, 0);
+//        t3dMesh = new Text3DMesh(
+//                text, fontName, realSize, false, realHeight, 0, 0);
     }
 
     @Override
     public List<Polygon> toPolygons() {
-        return new MeshRetriever(this).toCSG(noCenter).getPolygons();
+        //return new MeshRetriever(this).toCSG(noCenter).getPolygons();
+    	return null;
     }
 
     @Override
@@ -122,77 +118,77 @@ public class Text3d implements Primitive {
 
 }
 
-class MeshRetriever {
-
-    private final Text3d t3dMesh;
-
-    public MeshRetriever(Text3d t3dMesh) {
-        this.t3dMesh = t3dMesh;
-    }
-
-    public CSG toCSG(boolean noCenter) {
-        List<CSG> csgs = new ArrayList<>();
-
-        CSG result = null;
-
-        List<TexturedMesh> meshes = getMeshes();
-
-        for (int i = 0; i < meshes.size(); i++) {
-
-            TexturedMesh mesh = meshes.get(i);
-
-            CSG csg = MeshUtils.mesh2CSG(mesh);
-
-            double xTransform = mesh.getTransforms().stream().
-                    mapToDouble(tr -> tr.getTx()).sum();
-
-            csg = csg.transformed(Transform.unity().translateX(xTransform));
-
-            // rescale final mesh (see scaleFactor docs)
-            double xScale = 1.0 / t3dMesh.scaleFactor;
-            csg = csg.transformed(Transform.unity().
-                    scale(xScale, -xScale, xScale));
-
-            if (result == null) {
-                result = csg;
-            } else {
-                result = result.dumbUnion(csg);
-            }
-        }
-
-        if (!noCenter) {
-            result = result.transformed(
-                    Transform.unity().translate(
-                            -result.getBounds().getBounds().x * 0.5,
-                            -result.getBounds().getBounds().y * 0.5,
-                            -result.getBounds().getBounds().z * 0.5)
-            );
-        }
-
-        return result;
-    }
-
-    public List<TexturedMesh> getMeshes() {
-        try {
-            Field field = Text3DMesh.class.getDeclaredField("meshes");
-
-            field.setAccessible(true);
-
-            return (List<TexturedMesh>) field.get(t3dMesh.t3dMesh);
-        } catch (NoSuchFieldException ex) {
-            Logger.getLogger(MeshRetriever.class.getName()).
-                    log(Level.SEVERE, null, ex);
-        } catch (SecurityException ex) {
-            Logger.getLogger(MeshRetriever.class.getName()).
-                    log(Level.SEVERE, null, ex);
-        } catch (IllegalArgumentException ex) {
-            Logger.getLogger(MeshRetriever.class.getName()).
-                    log(Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            Logger.getLogger(MeshRetriever.class.getName()).
-                    log(Level.SEVERE, null, ex);
-        }
-
-        return null;
-    }
-}
+//class MeshRetriever {
+//
+//    private final Text3d t3dMesh;
+//
+//    public MeshRetriever(Text3d t3dMesh) {
+//        this.t3dMesh = t3dMesh;
+//    }
+//
+//    public CSG toCSG(boolean noCenter) {
+//        List<CSG> csgs = new ArrayList<>();
+//
+//        CSG result = null;
+//
+//        List<TexturedMesh> meshes = getMeshes();
+//
+//        for (int i = 0; i < meshes.size(); i++) {
+//
+//            TexturedMesh mesh = meshes.get(i);
+//
+//            CSG csg = MeshUtils.mesh2CSG(mesh);
+//
+//            double xTransform = mesh.getTransforms().stream().
+//                    mapToDouble(tr -> tr.getTx()).sum();
+//
+//            csg = csg.transformed(Transform.unity().translateX(xTransform));
+//
+//            // rescale final mesh (see scaleFactor docs)
+//            double xScale = 1.0 / t3dMesh.scaleFactor;
+//            csg = csg.transformed(Transform.unity().
+//                    scale(xScale, -xScale, xScale));
+//
+//            if (result == null) {
+//                result = csg;
+//            } else {
+//                result = result.dumbUnion(csg);
+//            }
+//        }
+//
+//        if (!noCenter) {
+//            result = result.transformed(
+//                    Transform.unity().translate(
+//                            -result.getBounds().getBounds().x * 0.5,
+//                            -result.getBounds().getBounds().y * 0.5,
+//                            -result.getBounds().getBounds().z * 0.5)
+//            );
+//        }
+//
+//        return result;
+//    }
+//
+//    public List<TexturedMesh> getMeshes() {
+//        try {
+//            Field field = Text3DMesh.class.getDeclaredField("meshes");
+//
+//            field.setAccessible(true);
+//
+//            return (List<TexturedMesh>) field.get(t3dMesh.t3dMesh);
+//        } catch (NoSuchFieldException ex) {
+//            Logger.getLogger(MeshRetriever.class.getName()).
+//                    log(Level.SEVERE, null, ex);
+//        } catch (SecurityException ex) {
+//            Logger.getLogger(MeshRetriever.class.getName()).
+//                    log(Level.SEVERE, null, ex);
+//        } catch (IllegalArgumentException ex) {
+//            Logger.getLogger(MeshRetriever.class.getName()).
+//                    log(Level.SEVERE, null, ex);
+//        } catch (IllegalAccessException ex) {
+//            Logger.getLogger(MeshRetriever.class.getName()).
+//                    log(Level.SEVERE, null, ex);
+//        }
+//
+//        return null;
+//    }
+//}
