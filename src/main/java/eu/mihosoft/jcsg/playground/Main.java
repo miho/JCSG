938de1f857b416b1855bf86ed9cf4a904d6cba58
Michael Hoffer <info@michaelhoffer.de>
2017-02-07 23:11:41 +0100

    splitting works (but more cases should be excluded)

--- src/main/java/eu/mihosoft/jcsg/playground/Main.java
+++ src/main/java/eu/mihosoft/jcsg/playground/Main.java
@@ -10,8 +10,10 @@ import eu.mihosoft.jcsg.CSG;
 import eu.mihosoft.jcsg.Polygon;
 import eu.mihosoft.jcsg.Sphere;
 import eu.mihosoft.jcsg.Vertex;
-import eu.mihosoft.vvecmath.Plane;
 import eu.mihosoft.vvecmath.Vector3d;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,28 +25,41 @@ public class Main {
 
     public static final double EPS = 1e-8;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         CSG c1 = new Sphere(Vector3d.zero(), 1.0, 32, 32).toCSG();
         CSG c2 = new Sphere(Vector3d.x(0.75), 1.0, 32, 32).toCSG();
 
-        SplitResult result = splitPolygons(c1.getPolygons(), c2.getPolygons());
+        SplitResult result1 = splitPolygons(c1.getPolygons(), c2.getPolygons(),
+                c1.getBounds(), c2.getBounds());
+        SplitResult result2 = splitPolygons(c2.getPolygons(), c1.getPolygons(),
+                c2.getBounds(), c1.getBounds());
+
+        result1.p2.addAll(result2.p2);
+
+        CSG result = CSG.fromPolygons(result1.p2);
+
+        Files.write(Paths.get("test.stl"), result.toStlString().getBytes());
 
     }
 
-    public static SplitResult splitPolygons(List<Polygon> ps1, List<Polygon> ps2, Bounds b1, Bounds b2) {
-        SplitResult result = new SplitResult();
+    public static SplitResult splitPolygons(
+            List<Polygon> ps1,
+            List<Polygon> ps2,
+            Bounds b1, Bounds b2) {
 
+        SplitResult result = new SplitResult();
         List<Polygon> ps2WithCuts = new ArrayList<>(ps2);
-        
+
         for (Polygon p1 : ps1) {
 
             // return early if polygon bounds do not intersect object bounds
-            if (p1.getBounds().intersects(b2)) {
+            if (!p1.getBounds().intersects(b2)) {
                 continue;
             }
-            
+
             List<Polygon> cutsWithP1 = new ArrayList<>();
+            List<Polygon> p2ToDelete = new ArrayList<>();
             for (Polygon p2 : ps2WithCuts) {
 
                 // return early if polygon bounds do not intersect other polygon bound
@@ -52,26 +67,25 @@ public class Main {
                     continue;
                 }
 
-//                // compute distance of polygon 1 vertices to polygon 2 plane
-//                int[] typesP1 = new int[p1.vertices.size()];
-//
-//                boolean typesEqual1 = true;
-//                int prevType1 = 0;
-//                for (int i = 0; i < p1.vertices.size(); i++) {
-//                    typesP1[i] = p2.plane.compare(p1.vertices.get(i).pos, EPS);
-//
-//                    if (i > 0 && typesEqual1) {
-//                        typesEqual1 &= prevType1 == typesP1[i];
-//                    }
-//
-//                    prevType1 = typesP1[i];
-//                }
-//
-//                // planes do not intersect, thus polygons do not intersect
-//                if (typesEqual1) {
-//                    continue;
-//                }
+                // compute distance of polygon 1 vertices to polygon 2 plane
+                int[] typesP1 = new int[p1.vertices.size()];
 
+                boolean typesEqual1 = true;
+                int prevType1 = 0;
+                for (int i = 0; i < p1.vertices.size(); i++) {
+                    typesP1[i] = p2.plane.compare(p1.vertices.get(i).pos, EPS);
+
+                    if (i > 0 && typesEqual1) {
+                        typesEqual1 &= prevType1 == typesP1[i];
+                    }
+
+                    prevType1 = typesP1[i];
+                }
+
+                // planes do not intersect, thus polygons do not intersect
+                if (typesEqual1) {
+                    continue;
+                }
                 // compute distance of polygon 2 vertices to polygon 1 plane
                 int[] typesP2 = new int[p2.vertices.size()];
 
@@ -93,9 +107,11 @@ public class Main {
                     continue;
                 }
                 
+                p2ToDelete.add(p2);
+
                 // cut p2 with plane1
                 List<Vector3d> frontPolygon = new ArrayList<>();
-                List<Vector3d> b = new ArrayList<>();
+                List<Vector3d> backPolygon = new ArrayList<>();
                 for (int i = 0; i < p2.vertices.size(); i++) {
                     int j = (i + 1) % p2.vertices.size();
                     int ti = typesP2[i];
@@ -106,39 +122,37 @@ public class Main {
                         frontPolygon.add(vi.pos);
                     }
                     if (ti != 1 /*back*/) {
-                        b.add(ti != BACK ? vi.clone() : vi);
+                        backPolygon.add(vi.pos);
                     }
-                    if (ti !=0 && tj != 1 && ti!=tj /*spanning*/) {
+                    if (ti != tj && (ti != 0 && tj != 0)/*spanning*/) {
                         double t = (p1.plane.getDist() - p1.plane.normal.dot(vi.pos))
                                 / p1.plane.normal.dot(vj.pos.minus(vi.pos));
                         Vertex v = vi.interpolate(vj, t);
-                        frontPolygon.add(v);
-                        b.add(v.clone());
+                        frontPolygon.add(v.pos);
+                        backPolygon.add(v.pos);
                     }
                 }
-                
-                cutsWithP1.add(Polygon.fromPoints(frontPolygon));
-                
-                if (frontPolygon.size() >= 3) {
-                    front.add(new Polygon(f, polygon.getStorage()));
-                }
-                if (b.size() >= 3) {
-                    back.add(new Polygon(b, polygon.getStorage()));
-                }
 
-                
-
+                if (frontPolygon.size() > 2) {
+                    cutsWithP1.add(Polygon.fromPoints(frontPolygon, p1.getStorage()));
+                }
+                if (backPolygon.size() > 2) {
+                    cutsWithP1.add(Polygon.fromPoints(backPolygon, p1.getStorage()));
+                }
             }
+            ps2WithCuts.addAll(cutsWithP1);
+             ps2WithCuts.removeAll(p2ToDelete);
         }
+        result.p1 = ps1;
+        result.p2 = ps2WithCuts;
 
         return result;
     }
 
     public static class SplitResult {
 
-        List<Polygon> inside1 = new ArrayList<>();
-        List<Polygon> inside2 = new ArrayList<>();
-        List<Polygon> outside = new ArrayList<>();
+        List<Polygon> p1 = new ArrayList<>();
+        List<Polygon> p2 = new ArrayList<>();
 
     }
 
