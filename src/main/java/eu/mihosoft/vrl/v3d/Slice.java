7dcf3f33e0706afffc6ab9eb777e203e0e05749c
Kevin Kohls <kohls.kevin.a@gmail.com>
2017-03-30 22:32:22 -0400

    Added Polygon Generator in Slicer
    I have no way of testing this. I know, it is complicated... I have a
    flowchart.

--- src/main/java/eu/mihosoft/vrl/v3d/Slice.java
+++ src/main/java/eu/mihosoft/vrl/v3d/Slice.java
@@ -9,10 +9,10 @@ import javax.vecmath.Matrix4d;
 import eu.mihosoft.vrl.v3d.ext.org.poly2tri.DelaunayTriangle;
 import eu.mihosoft.vrl.v3d.ext.org.poly2tri.PolygonUtil;
 
-public class Slice {	
+public class Slice {
 	private static ISlice sliceEngine = (incoming, slicePlane, normalInsetDistance) -> {
 		double COINCIDENCE_TOLERANCE = 0.0001;
-		List<Polygon> polygons = new ArrayList<>();
+		List<Polygon> rawPolygons = new ArrayList<>();
 
 		// Invert the incoming transform
 		Matrix4d inverse = slicePlane.scale(1.0D / slicePlane.getScale()).getInternalMatrix();
@@ -27,18 +27,19 @@ public class Slice {
 
 		// Loop over each polygon in the slice of the incoming CSG
 		// Add the polygon to the final slice if it lies entirely in the z plane
-		polygons.addAll(incoming.intersect(planeCSG).getPolygons().stream().filter(Slice::isPolygonAtZero)
+		rawPolygons.addAll(incoming.intersect(planeCSG).getPolygons().stream().filter(Slice::isPolygonAtZero)
 				.collect(Collectors.toList()));
-		
+
 		/* Convert the list of polygons to a list of triangles */
 		List<Polygon> triangles = new ArrayList<>();
-		for (int i=0;i<polygons.size();i++) {
-			eu.mihosoft.vrl.v3d.ext.org.poly2tri.Polygon p = PolygonUtil.fromCSGPolygon(polygons.get(i));
+		for (int i = 0; i < rawPolygons.size(); i++) {
+			eu.mihosoft.vrl.v3d.ext.org.poly2tri.Polygon p = PolygonUtil.fromCSGPolygon(rawPolygons.get(i));
 			eu.mihosoft.vrl.v3d.ext.org.poly2tri.Poly2Tri.triangulate(p);
 			List<DelaunayTriangle> t = p.getTriangles();
-			for (int j=0;j<t.size();j++) triangles.add(t.get(j).toPolygon());
+			for (int j = 0; j < t.size(); j++)
+				triangles.add(t.get(j).toPolygon());
 		}
-		
+
 		/* List every edge */
 		List<Edge> edges = new ArrayList<>();
 		for (Polygon t : triangles) {
@@ -46,14 +47,14 @@ public class Slice {
 			edges.add(new Edge(t.vertices.get(1), t.vertices.get(2)));
 			edges.add(new Edge(t.vertices.get(2), t.vertices.get(0)));
 		}
-		
+
 		/* Remove internal edges */
-		for (int i=0;i<edges.size();i++) {
+		for (int i = 0; i < edges.size(); i++) {
 			boolean match = false;
-			for (int j=0;j<edges.size() || match;j++) {
-				if (i!=j &&
-						edges.get(i).getP1().pos.minus(edges.get(j).getP2().pos).magnitude()<=COINCIDENCE_TOLERANCE &&
-						edges.get(i).getP2().pos.minus(edges.get(j).getP1().pos).magnitude()<=COINCIDENCE_TOLERANCE) {
+			for (int j = 0; j < edges.size() && !match; j++) {
+				if (edges.get(i).getP1().pos.minus(edges.get(j).getP2().pos).magnitude() <= COINCIDENCE_TOLERANCE
+						&& edges.get(i).getP2().pos.minus(edges.get(j).getP1().pos)
+								.magnitude() <= COINCIDENCE_TOLERANCE) {
 					edges.remove(i);
 					edges.remove(j);
 					i--;
@@ -61,7 +62,23 @@ public class Slice {
 				}
 			}
 		}
-		
+
+		/* Generate polygons from edges */
+		List<Polygon> polygons = new ArrayList<>();
+		for (int edgeIndex = 0; edges.size() > 0;) {
+			List<Vertex> vertices = new ArrayList<>();
+			vertices.add(edges.get(0).getP1());
+			for (; edges.get(edgeIndex).getP2().pos.minus(vertices.get(0).pos).magnitude() <= COINCIDENCE_TOLERANCE;) {
+				vertices.add(edges.get(edgeIndex).getP2());
+				edges.remove(edgeIndex);
+				for (; edgeIndex < edges.size() && !(vertices.get(vertices.size() - 1).pos
+						.minus(edges.get(edgeIndex).getP1().pos).magnitude() <= COINCIDENCE_TOLERANCE); edgeIndex++)
+					;
+			}
+			edges.remove(edgeIndex);
+			polygons.add(new Polygon(vertices));
+		}
+
 		return polygons;
 	};
 
