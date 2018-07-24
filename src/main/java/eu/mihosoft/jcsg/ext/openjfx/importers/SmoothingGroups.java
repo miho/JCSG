0ac8acb555f1862139785212645f4ddfce27682c
Michael Hoffer <info@michaelhoffer.de>
2018-07-24 20:22:41 +0200

    Compiles with JDK 10

--- src/main/java/eu/mihosoft/jcsg/ext/openjfx/importers/SmoothingGroups.java
+++ src/main/java/eu/mihosoft/jcsg/ext/openjfx/importers/SmoothingGroups.java
@@ -40,7 +40,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
-import com.sun.javafx.geom.Vec3f;
 import javafx.scene.shape.TriangleMesh;
 
 /** Util for converting Normals to Smoothing Groups */
