9a071912b52d11c563fb00a56114838c38277ead
Kevin Harrington <mad.hephaestus@gmail.com>
2019-06-22 20:29:43 -0400

    jcsg javafx initializer playing nice with others

--- src/main/java/eu/mihosoft/vrl/v3d/JavaFXInitializer.java
+++ src/main/java/eu/mihosoft/vrl/v3d/JavaFXInitializer.java
@@ -2,23 +2,31 @@ package eu.mihosoft.vrl.v3d;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-
 public class JavaFXInitializer extends javafx.application.Application {
 	private static final int NUM_COUNT = 2;
-	public final static java.util.concurrent.CountDownLatch latch = new java.util.concurrent.CountDownLatch(NUM_COUNT);
+	private final static java.util.concurrent.CountDownLatch latch = new java.util.concurrent.CountDownLatch(NUM_COUNT);
+	
 	public JavaFXInitializer(){
 		
 	}
 	private static void gointernal() {
-		if(latch.getCount()<NUM_COUNT) {
+		if(latch.getCount()!=NUM_COUNT) {
 			//System.out.println("ERR initializer already started");
 			return;
 		}
-		System.out.println("Starting JavaFX initializer...");
+		System.out.println("Starting JavaFX initializer..."+JavaFXInitializer.class);
 		latch.countDown();
-		launch();
+		try {
+			launch();
+		}catch(java.lang.IllegalStateException e) {
+			latch.countDown();
+		}
 	}
 	public static void go() {
+		if(latch.getCount()!=NUM_COUNT) {
+			//System.out.println("ERR initializer already started");
+			return;
+		}
 		new Thread() {
 			public void run() {
 				gointernal();
@@ -30,7 +38,9 @@ public class JavaFXInitializer extends javafx.application.Application {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println("Finished JavaFX initializing!");
+		StackTraceElement[] stacktrace = Thread.currentThread().getStackTrace();
+		StackTraceElement e = stacktrace[2];//maybe this number needs to be corrected
+		System.out.println("Finished JavaFX initializing! "+e);
 	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
