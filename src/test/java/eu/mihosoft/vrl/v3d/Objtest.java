975258dbd8de9cf359c0831fa3223b382869e699
Kevin Harrington <mad.hephaestus@gmail.com>
2020-05-28 12:57:28 -0400

    cleanup

--- src/test/java/eu/mihosoft/vrl/v3d/Objtest.java
+++ src/test/java/eu/mihosoft/vrl/v3d/Objtest.java
@@ -4,14 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 public class Objtest {
 
 	@Test
-	public void test() {
-		String s = new Cube (1).toCSG().toObjString()
+	public void test() throws IOException {
+		CSG csg = new Cube (1).toCSG();
+		csg.addDatumReference(new Transform());
+		
+		String s=csg.toObjString()
 				.split("# Faces")[1]
 				.split("# End")[0].trim();
 		assertFalse(s.length()<4);		
+		FileUtil.write(Paths.get("test.obj"),
+				csg.toObjString());
 	}
 
 }
