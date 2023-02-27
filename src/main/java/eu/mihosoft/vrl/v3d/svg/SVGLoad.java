a4d35f8296780bef6ceb07f69012de5b7cffa0a0
Kevin Harrington <mad.hephaestus@gmail.com>
2023-02-27 17:48:20 -0500

    just rotatte

--- src/main/java/eu/mihosoft/vrl/v3d/svg/SVGLoad.java
+++ src/main/java/eu/mihosoft/vrl/v3d/svg/SVGLoad.java
@@ -381,8 +381,7 @@ public class SVGLoad {
 		}else if (transformValue.contains("rotate")) {
 			String[] rotvals = transformValue.replaceAll("rotate", "").replaceAll("\\(", "")
 					.replaceAll("\\)", "").split("\\,");
-			newFrame.apply(new Transform().rotZ(Double.parseDouble(rotvals[0])).translate(toPx(rotvals[1]),
-					toPx(rotvals[2]), 0));
+			newFrame.rotZ(Double.parseDouble(rotvals[0]));
 
 		}  else if (transformValue.contains("scale")) {
 			String[] transformValues = transformValue.replaceAll("scale", "").replaceAll("\\(", "")
